4418f50cf1d3b1a2b1d23d0dc8a3a48a5dc6cc2d
Hassan Mahdee <hasanmahdee013@gmail.com>
2026-02-22 19:20:39 +0600

    html structure done, js logic loading

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
-console.log('hi');
+// all children of the "job-cards" will be counted then shown inside "all-stat"
+// all interviews will be counted from the [interviewList] and shown inside "interview-stat"
+// all rejected will be counted from the [rejectedList] and shown inside "rejected-stat"
+// clicking the "interview-btn" will add a copy of that job-card to [interviewList] and update the status of that job-card from "not applied/rejected" to "interviewing".
+// clicking the "rejected-btn" will add a copy of that job-card to [rejectedList] and update the status of that job-card from "not applied/interviewing" to "rejected".
+// clicking on "interview-tab" will create 
